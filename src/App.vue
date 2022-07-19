566870433bf9c46128fc193bc835bd095a6d6227
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-19 14:42:21 +0200

    AppHeader

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <AppHeader/>
+    <AppMain/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppMain,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <img src="https://play-lh.googleusercontent.com/UrY7BAZ-XfXGpfkeWg0zCCeo-7ras4DCoRalC_WXXWTK9q5b0Iw7B0YQMsVxZaNB7DM" alt="spotify-logo">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+header{
+    background-color: #2e3a46;
+}
+
+img{
+    height: 3rem;
+    margin: .8rem;
+}
+</style>
